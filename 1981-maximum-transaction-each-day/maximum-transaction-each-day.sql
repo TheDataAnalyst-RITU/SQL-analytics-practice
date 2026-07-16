79b2ff11cfd2ecf9653fad22b4766475e021e17f
Ritu Chaudhary <rituchaudhary2424@gmail.com>
2026-07-16 09:13:42 -0400

    Time: 4089 ms (5.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1981-maximum-transaction-each-day/maximum-transaction-each-day.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT transaction_id
+FROM (
+    SELECT *,
+           RANK() OVER(PARTITION BY DATE(day) ORDER BY amount DESC) rnk
+    FROM Transactions
+) t
+WHERE rnk = 1
+ORDER BY transaction_id;
