3e7a75044ddb57ed0db309c9d156c69012ae05d8
Ritu Chaudhary <rituchaudhary2424@gmail.com>
2026-07-22 19:38:55 -0400

    Time: 1018 ms (34.94%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT COUNT(*) AS accounts_count
+FROM Subscriptions
+WHERE YEAR(end_date) = 2021
+AND account_id NOT IN (
+    SELECT account_id
+    FROM Streams
+    WHERE YEAR(stream_date) = 2021
+);
