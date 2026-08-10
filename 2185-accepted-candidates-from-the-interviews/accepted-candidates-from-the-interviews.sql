64ada4d240ee191d3dfcfc8b6c062d1039a38539
Ritu Chaudhary <rituchaudhary2424@gmail.com>
2026-08-10 17:27:26 -0400

    Time: 1444 ms (18.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT c.candidate_id
+FROM Candidates c
+JOIN Rounds r ON c.interview_id = r.interview_id
+WHERE c.years_of_exp >= 2
+GROUP BY c.candidate_id
+HAVING SUM(r.score) > 15;
